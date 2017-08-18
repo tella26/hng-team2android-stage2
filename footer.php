62e287889b764a0fe22f44d84f18d66ccaf97c45
NsikakApp <nsikakthompson73@gmail.com>
2017-08-18 22:09:46 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <center><<h1>footer</h1></center>
+    </head>
+    <style>
+    body{
+         font-family: 'Lato', sans-serif;
+    }
+    </style>
+    <body>
+    
+    </body>
+</html>
