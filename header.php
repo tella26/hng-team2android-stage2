809c5863db9157238e062dde73e8b3eed11b2a06
NsikakApp <nsikakthompson73@gmail.com>
2017-08-19 00:39:37 +0100

    created header

--- header.php
+++ header.php
@@ -4,19 +4,63 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
         <script type="text/javascript" src="assets/js/jquery.js"></script>
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-        <center><<h1>header</h1></center>
+         <link  rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <style>
-    body{
-         font-family: 'Lato', sans-serif;
-    }
-    </style>
+    
     <body>
     
-    </body>
-</html>
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+            <img src="assets/images/logo.png" class="img-responsive" height="50px" width="160px" ></a>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            
+            <ul class="nav navbar-nav navbar-right">
+               
+                 <li class="nav-item">
+                    <a class="nav-link active" href="#">Contact</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link text-color" href="#">Join</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-color" href="#">Help</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  text-color" href="#">FAQ</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+
+    
+    <div class="jumbotron" style="background: #2753C2">
+        <div class="container">
+            <div class="text-center body-text" style="font-size: 50px">
+                
+            <b>Become a better Software Developer</b>
+            
+              </div>
+        </div>
+    </div>
+    
+     <div>
+       
+     </div>
+    
+      
+    
